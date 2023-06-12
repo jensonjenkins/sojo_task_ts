93b4753509ddab0f17d6529307f01f38fa8343f8
jensonjenkins <dave.martheen@gmail.com>
2023-06-12 22:03:03 +0800

    bug fix

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -1,8 +1,8 @@
 import './styles.css'
+import React from 'react'
 import logo from '../../images/sojo_logo.jpg'
 
 export default function NavBar() {
-
     return (
         <>
             <div className='TopBar'>
